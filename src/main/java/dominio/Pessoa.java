2108fe1869bc7326dcdaf7f2b4eb14bc0aa41539
Pedro H. B. Cavalcante <pedrinho.sjs@ufrn.edu.br>
2017-09-04 23:56:34 -0300

    alteração na classe pessoa

--- src/main/java/dominio/Pessoa.java
+++ src/main/java/dominio/Pessoa.java
@@ -1,79 +1,7 @@
 package dominio;
 
 public class Pessoa {
-	
-<<<<<<< Upstream, based on branch 'master' of https://github.com/pedrohcavalcante/Condominium.git
-	public String getLogin() {
-		return login;
-	}
-	public void setLogin(String login) {
-		this.login = login;
-	}
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-	public int getTipoID() {
-		return tipoID;
-	}
-	public void setTipoID(int tipoID) {
-		this.tipoID = tipoID;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public String getBloco() {
-		return bloco;
-	}
-	public void setBloco(String bloco) {
-		this.bloco = bloco;
-	}
-	public String getApartamento() {
-		return apartamento;
-	}
-	public void setApartamento(String apartamento) {
-		this.apartamento = apartamento;
-	}
-	public String getVinculo() {
-		return vinculo;
-	}
-	public void setVinculo(String vinculo) {
-		this.vinculo = vinculo;
-	}
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	
-	
-	//criarlistadeconvidados(id);
-=======
+
 	private String nome;
 	private String cpf;
 	private String bloco;
@@ -134,7 +62,4 @@ public class Pessoa {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
->>>>>>> 1a33219 Criação do enum tipo pessoa
 }
