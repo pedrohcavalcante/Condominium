74519a33a3a82748228500fd8f41f859cb14c501
Pedro H. B. Cavalcante <pedrinho.sjs@ufrn.edu.br>
2017-09-05 00:03:11 -0300

    ajeitando lista de convidados

--- src/main/java/dominio/ListaConvidados.java
+++ src/main/java/dominio/ListaConvidados.java
@@ -1,14 +1,6 @@
 package dominio;
 
 public class ListaConvidados {
-<<<<<<< Upstream, based on branch 'master' of https://github.com/pedrohcavalcante/Condominium.git
-	public String nome;
-	public String bloco;
-	public String apartamento;
-	public String idMorador;
-	
-	//finalizar();
-=======
 	
 	private int idLista;
 	private int idMorador;
@@ -32,6 +24,4 @@ public class ListaConvidados {
 	public void setNomePessoa(String nomePessoa) {
 		this.nomePessoa = nomePessoa;
 	}
-	
->>>>>>> 1a33219 Criação do enum tipo pessoa
 }
