187091d0236396c0b8d90ff763ddedbba0a6463d
Adelino Afonso <aafavelino@icloud.com>
2017-09-04 23:59:03 -0300

    Atual

--- src/main/java/dominio/ListaConvidados.java
+++ src/main/java/dominio/ListaConvidados.java
@@ -1,10 +1,28 @@
 package dominio;
 
 public class ListaConvidados {
-	public String nome;
-	public String bloco;
-	public String apartamento;
-	public String idMorador;
-	
-	//finalizar();
+ 
+ private int idLista;
+ private int idMorador;
+ private String nomePessoa;
+ 
+ public int getIdLista() {
+  return idLista;
+ }
+ public void setIdLista(int idLista) {
+  this.idLista = idLista;
+ }
+ public int getIdMorador() {
+  return idMorador;
+ }
+ public void setIdMorador(int idMorador) {
+  this.idMorador = idMorador;
+ }
+ public String getNomePessoa() {
+  return nomePessoa;
+ }
+ public void setNomePessoa(String nomePessoa) {
+  this.nomePessoa = nomePessoa;
+ }
+ 
 }
