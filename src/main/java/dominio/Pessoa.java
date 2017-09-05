edb186447c4f64888402281dcdb79cf870233ed5
Adelino Afonso <aafavelino@icloud.com>
2017-09-04 22:27:31 -0300

    Variáveis da classe pessoa

--- src/main/java/dominio/Pessoa.java
+++ src/main/java/dominio/Pessoa.java
@@ -1,5 +1,17 @@
 package dominio;
 
 public class Pessoa {
-
+	public String login;
+	protected String senha;
+	public int tipoID;
+	public String nome;
+	public String cpf;
+	public String bloco;
+	public String apartamento;
+	public String vinculo;
+	public String telefone;
+	public int id;
+	public String email;
+	
+	//criarlistadeconvidados(id);
 }
