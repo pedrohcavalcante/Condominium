1150ff783fe3ef319b3a22a70c157d2c950bb97e
Adelino Afonso <aafavelino@icloud.com>
2017-09-05 16:44:31 -0300

    Email

--- src/main/java/dao/JavaMailApp.java
+++ src/main/java/dao/JavaMailApp.java
@@ -9,46 +9,12 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import controle.JavaMailAppMBean;
+
 
 public class JavaMailApp {
 	 public static void main(String[] args) {
-         Properties props = new Properties();
-         /** Parâmetros de conexão com servidor Gmail */
-         props.put("mail.smtp.host", "smtp.gmail.com");
-         props.put("mail.smtp.socketFactory.port", "465");
-         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-         props.put("mail.smtp.auth", "true");
-         props.put("mail.smtp.port", "465");
-
-         Session session = Session.getDefaultInstance(props,
-                     new javax.mail.Authenticator() {
-                          protected PasswordAuthentication getPasswordAuthentication()
-                          {
-                                return new PasswordAuthentication("adelino18fernandes@gmail.com", "adelino01");
-                          }
-                     });
-
-         /** Debug para sessão */
-         session.setDebug(true);
-
-         try {
-
-               Message message = new MimeMessage(session);
-               message.setFrom(new InternetAddress("adelino18fernandes8@gmail.com")); //Remetente
-
-               Address[] recipientes = InternetAddress //Destinatário(s)
-                          .parse("aafavelino@icloud.com, adelino-afonso@hotmail.com, Pedrohbcavalcante@outlook.com");  
-
-               message.setRecipients(Message.RecipientType.TO, recipientes);
-               message.setSubject("Enviando email com JavaMail");//Assunto
-               message.setText("Enviei este email utilizando JavaMail com minha conta GMail!");
-               /**Enviar a mensagem criada*/
-               Transport.send(message);
-
-               System.out.println("Feito!!!");
-
-          } catch (MessagingException e) {
-               throw new RuntimeException(e);
-         }
+		 JavaMailAppMBean email =  new JavaMailAppMBean();
+		 email.enviarEmail("Teste", "Uma mensagem qualquer");
    }
 }
