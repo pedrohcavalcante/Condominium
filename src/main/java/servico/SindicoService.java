419aaf45e884586b8376201501633a11b7755b5d
Adelino Afonso <aafavelino@icloud.com>
2017-09-06 09:11:17 -0300

    sindico

--- src/main/java/servico/SindicoService.java
+++ src/main/java/servico/SindicoService.java
@@ -1,5 +1,39 @@
 package servico;
 
-public class SindicoService {
+import java.util.List;
+
+import dominio.Pessoa;
+
+public class SindicoService implements InterfaceServico<Pessoa>{
+
+	@Override
+	public List<Pessoa> buscarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pessoa buscar(Pessoa elemento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void inserir(Pessoa novo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Pessoa elemento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remover(Pessoa elemento) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
