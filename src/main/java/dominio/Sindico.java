105bad0715b01fbc409fa3edcdbcc5b02bd65c83
Pedro H. B. Cavalcante <pedrinho.sjs@ufrn.edu.br>
2017-09-19 08:31:05 -0300

    Criação dos campos referentes ao sindico

--- src/main/java/dominio/Sindico.java
+++ src/main/java/dominio/Sindico.java
@@ -1,8 +1,107 @@
 package dominio;
 
 public class Sindico {
-	public int tipoID;
 	
-	//cadastraPessoa();
-	//cadastraCorrespondencia();
+	private int id;
+	private String nome;
+	private String login;
+	private String senha;
+	private TipoPessoa tipoID;
+	private String CPF;
+	private String bloco;
+	private String apartamento;
+	private String vinculo;
+	private String telefone;
+	private String email;
+	
+	public Sindico() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public TipoPessoa getTipoID() {
+		return tipoID;
+	}
+
+	public void setTipoID(TipoPessoa sindico) {
+		this.tipoID = sindico;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+
+	public String getBloco() {
+		return bloco;
+	}
+
+	public void setBloco(String bloco) {
+		this.bloco = bloco;
+	}
+
+	public String getApartamento() {
+		return apartamento;
+	}
+
+	public void setApartamento(String apartamento) {
+		this.apartamento = apartamento;
+	}
+
+	public String getVinculo() {
+		return vinculo;
+	}
+
+	public void setVinculo(String vinculo) {
+		this.vinculo = vinculo;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
