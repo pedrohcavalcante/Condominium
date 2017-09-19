e7a2ce8accaa11343f119d1b2007d0624e11f312
Pedro H. B. Cavalcante <pedrinho.sjs@ufrn.edu.br>
2017-09-19 08:28:40 -0300

    Atualização do UsuarioDAO
    Para que se possa utilizar esta opção utilizando informações provenientes do banco de dados, realizar alterações que estão comentadas.

--- src/main/java/dao/UsuarioDAO.java
+++ src/main/java/dao/UsuarioDAO.java
@@ -7,100 +7,59 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-//import org.primefaces.model.diagram.Connection;
-
 import dominio.TipoPessoa;
 import dominio.Usuario;
-import dao.GerenciarConexao;
+import dao.GerenciadorConexao;
 
-public class UsuarioDAO implements InterfaceDAO<Usuario>{
-
-	@Override
-	public List<Usuario> buscarTodos() {
-		List<Usuario> resultado = new ArrayList<Usuario>();
-		Connection conexaoDAO = GerenciarConexao.getConexao();
-		String sql = "SELECT * FROM Usuario";
-		PreparedStatement ps;
-		try {
-			ps = (conexaoDAO).prepareStatement(sql);
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
-				Usuario usuario = new Usuario();
-				usuario.setLogin(rs.getString("login"));
-				usuario.setSenha(rs.getString("senha"));
-				usuario.setTipoID(TipoPessoa.fromInteger(rs.getInt("tipoId")));
-				resultado.add(usuario);
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return resultado;
-	}
-
-	@Override
-	public Usuario buscar(Usuario elemento) {
-		Usuario resultado = new Usuario();
-		Connection con = GerenciarConexao.getConexao();
-		String sql = "SELECT * FROM Usuario WHERE matricula='"+elemento.getLogin()+"';";
-		try {
-			PreparedStatement ps = (con).prepareStatement(sql);
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
-				resultado.setLogin(rs.getString("login"));
-				resultado.setSenha(rs.getString("senha"));
-				resultado.setTipoID(TipoPessoa.fromInteger(rs.getInt("tipoId")));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return resultado;
-	}
+public class UsuarioDAO implements AllDAO<Usuario>{
 	
-	public Usuario buscar(String login){
-		Usuario resultado = new Usuario();
-		Connection con = GerenciarConexao.getConexao();
-		String sql = "SELECT * FROM Usuario WHERE matricula='"+login+"';";
-		try {
-			PreparedStatement ps = (con).prepareStatement(sql);
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
-				resultado.setLogin(rs.getString("login"));
-				resultado.setSenha(rs.getString("senha"));
-				resultado.setTipoID(TipoPessoa.fromInteger(rs.getInt("tipoId")));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	private List<Usuario> resultadoConsulta;
+	//private Connection conexaoBanco;
+	//private ResultSet resultados_consulta;
+	Usuario getInfo;
 
-		return resultado;
+	public List<Usuario> buscarTodos() {
+		//resultadoConsulta = new ArrayList<Usuario>();
+		//conexaoBanco = GerenciadorConexao.getConexao();
+		//String seleciona_todos_usuarios = "SELECT * FROM USUARIO";
+		//try {
+			//PreparedStatement query = conexaoBanco.prepareStatement(seleciona_todos_usuarios);
+			//resultados_consulta = query.executeQuery();
+			/*while (resultados_consulta.next()){
+				getInfo = new Usuario();
+				getInfo.setLogin(resultados_consulta.getString("login"));
+				getInfo.setSenha(resultados_consulta.getString("senha"));
+				getInfo.setID(TipoPessoa.fromInteger(resultados_consulta.getInt("tipoID")));
+				resultadoConsulta.add(getInfo);
+			}*/
+			
+		//} catch (SQLException e) {
+			getInfo = new Usuario();
+			resultadoConsulta = new ArrayList<Usuario>();
+			getInfo.setLogin("admin");
+			getInfo.setSenha("admin");
+			getInfo.setID(TipoPessoa.sindico);
+			resultadoConsulta.add(getInfo);
+			//e.printStackTrace();
+		//}
+		return resultadoConsulta;
 	}
 
-	@Override
+	public Usuario buscar(Usuario elemento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	public void inserir(Usuario novo) {
-		Connection con = GerenciarConexao.getConexao();
-		String sql = "INSERT INTO Usuario VALUES (?, ?)";
-		try {
-			PreparedStatement ps = (con).prepareStatement(sql);
-			ps.setString(1, novo.getLogin());
-			ps.setString(2, novo.getSenha());
-			ps.setInt(3, novo.getTipoID().ordinal());
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void atualizar(Usuario elemento) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void remover(Usuario elemento) {
 		// TODO Auto-generated method stub
 		
